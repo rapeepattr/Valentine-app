279df49ac6b192ef2a63f6bc30d3b4bec75395af
rapeepattr <rapeepat.ptpk@gmail.com>
2024-02-08 18:28:47 +0700

    First MVP

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useState } from "react";
+import './App.css'
+// import Landing from "./components/Landing";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+   const [state, setState] = useState(1);
+   const [click, setClick] = useState(false);
+
+   function mouseOver() {
+      if (state === 1) {
+         setState((prevVal) => (prevVal += 2));
+      } else if (state === 2) {
+         setState((prevVal) => (prevVal += 2));
+      } else if (state === 3) {
+         setState((prevVal) => (prevVal -= 2));
+      }
+   }
+
+   var mystyle = {
+      left:
+         state === 1 ? "43vw" : state === 2 ? "25vw" : state === 3 ? "15vw" : "", position: "absolute",
+   };
+
+   return (
+      <>
+         {!click &&
+            <div className="mx-auto my-40">
+               <img src="https://media.tenor.com/1jrUvITt1QcAAAAi/github-sticker.gif" className="flex justify-center h-60 mb-10 mx-auto">
+               </img>
+               <p className="font-bold text-3xl text-center mb-10 text-pink-600">
+                  Will you be my Valentine ?
+               </p>
+
+               <div className="flex gap-96 align-middle justify-center items-center">
+                  <button onMouseOver={mouseOver} style={mystyle} type="button" class="font-bold text-xl px-5 py-3 run-btn text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 rounded-lg text-center me-2 mb-2">
+                     No ;(
+                  </button>
+
+                  <button onClick={() => setClick(true)} type="button" class="font-bold text-xl ml-20 px-5 py-3 text-white bg-gradient-to-r from-pink-400 via-pink-500 to-pink-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-pink-300 dark:focus:ring-pink-800 rounded-lg text-center me-2 mb-2">
+                     Yess!
+                  </button>
+               </div>
+            </div>
+         }
+         {
+            click &&
+            <div className="mx-auto my-40">
+               <img src="https://media.tenor.com/7Ie2NGeUuFgAAAAi/cutecat.gif" className="flex justify-center h-60 mb-10 mx-auto">
+               </img>
+               <p className="font-bold text-3xl text-center mb-10 text-pink-600">
+                  Yayyyyyyyyy !!!
+               </p>
+            </div>
+         }
+      </>
+   );
 }
-
-export default App;
---
+++ src/components/Landing.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import { useState } from "react";
+import './Landing.css'
+
+const Landing = () => {
+    const [state, setState] = useState(1);
+
+    function mouseOver() {
+      if (state === 1) {
+        setState((prevVal) => (prevVal += 2));
+      } else if (state === 2) {
+        setState((prevVal) => (prevVal += 2));
+      } else if (state === 3) {
+        setState((prevVal) => (prevVal -= 2));
+      }
+    }
+  
+    var mystyle = {
+      left:
+        state === 1 ? "43vw" : state === 2 ? "25vw" : state === 3 ? "15vw" : "", position: "absolute",
+    };
+
+    return (
+        <div className="mx-auto my-40">
+            <img src="https://media.tenor.com/1jrUvITt1QcAAAAi/github-sticker.gif" className="flex justify-center h-60 mb-10 mx-auto">
+            </img>
+            <p className="font-bold text-3xl text-center mb-10 text-pink-600">
+                Will you be my Valentine ?
+            </p>
+
+            <div className="flex gap-96 align-middle justify-center items-center">
+                <button onMouseOver={mouseOver} style={mystyle} type="button" class="font-bold text-xl px-5 py-3 run-btn text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 rounded-lg text-center me-2 mb-2">
+                    No ;(
+                </button>
+
+                <button type="button" class="font-bold text-xl ml-20 px-5 py-3 text-white bg-gradient-to-r from-pink-400 via-pink-500 to-pink-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-pink-300 dark:focus:ring-pink-800 rounded-lg text-center me-2 mb-2">
+                    Yess!
+                </button>
+            </div>
+
+        </div>
+    )
+}
+
+export default Landing
